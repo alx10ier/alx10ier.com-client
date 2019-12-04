96362a5f2d0b48bbd2866734dfb2ad1eda7e00b6
Alx10ier <alx10ier@gmail.com>
2019-12-04 10:58:23 -0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  app-header
+  router-view
 </template>
+
+<script>
+import Header from "@/components/Header.vue"
+
+export default {
+  components: {
+    "app-header": Header
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.header
+  .navbar
+    .navbar-menu
+      router-link.navbar-item(to='/') Home
+      router-link.navbar-item(to='/about') About
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+a {
+  font-weight: bold;
+  color: #2c3e50;
+
+  &.router-link-exact-active {
+    color: #42b983;
+  }
+}
+</style>
