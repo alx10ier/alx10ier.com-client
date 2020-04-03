fb2dc37e5dea74344902237790c95f86048f0189
Alx10ier <alx10ier@gmail.com>
2020-04-03 15:29:35 -0700

    test change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <div>{{ res }}</div>
+    <div>Test Change</div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
